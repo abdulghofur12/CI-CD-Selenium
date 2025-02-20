85fd581bec4c261aafe731cea9792e702467d751
abdulghofur12 <ghofursans12@gmail.com>
2025-02-20 20:59:53 +0700

    menambahkan semua syntax

--- src/test/java/ghofur/stepdef/CucumberHooks.java
+++ src/test/java/ghofur/stepdef/CucumberHooks.java
@@ -1,4 +1,18 @@
 package ghofur.stepdef;
 
-public class CucumberHooks {
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import ghofur.stepdef.BaseTest;
+
+public class CucumberHooks extends BaseTest {
+
+    @Before
+    public void beforeTest(){
+        getDriver();
+    }
+    @After
+    public void  AfterTest(){
+        driver.close();
+
+    }
 }
